afd296549a6fa481eab09c034936c3e5ae036578
Ryyan Choudhary <134947247+Ryyan-Choudhary@users.noreply.github.com>
2024-11-12 21:01:09 +0500

    Clearing screen and making structure

--- Ping_Pong_23L-2542.asm
+++ Ping_Pong_23L-2542.asm
@@ -1,3 +1,45 @@
 ;Project by Ryyan Choudhary() and Usama Zafar()
 ;Ping Pong Game in Assembly
+[org 0x100]
+jmp mainPhase
+
+clrscrntoBlack:
+push bp
+mov bp,sp
+push es 
+push ax 
+push cx 
+push di 
+
+mov ax,0xB800
+mov es,ax
+xor di,di 
+mov ax,0x0720
+mov cx,2000
+
+cld 
+rep stosw 
+
+pop di 
+pop cx 
+pop ax 
+pop es 
+pop bp 
+ret
+
+mainPhase:
+;code
+call clrscrntoBlack
+
+updatePhase:
+;code
+
+displayPhase:
+;code
+
+endGame:
+mov ax,0x4C00
+int 0x21
+
+
 
